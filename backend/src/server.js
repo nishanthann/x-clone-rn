eae0343c04719ead87f60d21378f2f3766c247ed
nishanthann <nizhanth23@gmail@gmail.com>
2025-07-18 00:32:29 +0530

    notication &arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import {clerkMiddleware} from "@clerk/express"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 const app =express();
 
@@ -20,6 +21,7 @@ app.get('/',(req,res)=>res.send("Hello from server"))
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 
 // error handling middleware
