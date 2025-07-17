2aaf614937ae9a1b38c374d481196213897fb7f5
nishanthann <nizhanth23@gmail@gmail.com>
2025-07-17 12:06:34 +0530

    user routes addd

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import {ENV} from "./config/env.js"
 import {connectDB } from './config/db.js';
+import cors from "cors"
+import {clerkMiddleware} from "@clerk/express"
+import userRoutes from "./routes/user.route.js"
+
+
 const app =express();
+
+app.use(cors());
+app.use(express.json())
+app.use(clerkMiddleware())
+
 
 connectDB();
 
 app.get('/',(req,res)=>res.send("Hello from server"))
+app.use("/api/users",userRoutes)
+
 
 app.listen(ENV.PORT,()=>console.log("server is up and running on PORT:",ENV.PORT))
